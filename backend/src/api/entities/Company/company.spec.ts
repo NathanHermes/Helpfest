7ff46052e4049bd5a0668f1e00f0977eb83cd1be
NathanHermes <hermesnathan8@gmail.com>
2023-09-25 20:42:45 -0300

    Test: Should be able to create a object instance of company

---
+++ backend/src/api/entities/Company/company.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest'
+import { Company } from './company'
+
+
+
+test('Should be create a object instance of company', () => {
+  const company = new Company({
+    name: 'Oasis Eventos',
+    email: 'oasis@gmail.com',
+    CNPJ: '56041364000174',
+    phone: '1136861256',
+    address: 'Rod. Washington Luís',
+    number: '',
+    city: 'São Carlos',
+    uf: 'SP',
+    complement: '',
+    secret: 'test'
+  })
+
+  expect(company).toBeInstanceOf(Company)
+})
