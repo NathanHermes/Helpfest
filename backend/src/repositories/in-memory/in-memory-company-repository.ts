efa67c0f41afa9538f327fe0af5e551f8be51990
NathanHermes <hermesnathan8@gmail.com>
2023-09-25 21:43:48 -0300

    Feat: Add create method of InMemoryCompanyRepository

---
+++ backend/src/repositories/in-memory/in-memory-company-repository.ts
@@ -0,0 +1,10 @@
+import { Company } from '../../api/entities/Company/company'
+import { CompanyRepository } from '../company-repository'
+
+export class InMemoryCompanyRepository implements CompanyRepository {
+  private companies: Company[] = []
+
+  async create (company: Company): Promise<number> {
+    return this.companies.push(company)
+  }
+}
