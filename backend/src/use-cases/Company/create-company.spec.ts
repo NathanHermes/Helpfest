4a74eb058a18d0b68da7d042200ba1824e8708ef
NathanHermes <hermesnathan8@gmail.com>
2023-10-05 22:47:09 -0300

    test(use-case): :white_check_mark: add blank cnpj validation test

--- backend/src/use-cases/Company/create-company.spec.ts
+++ backend/src/use-cases/Company/create-company.spec.ts
@@ -96,5 +96,10 @@ describe('Create company', () => {
       company.CNPJ = null
       expect(createCompany.execute(company)).rejects.toThrowError('Invalid company CNPJ')
     })
+
+    it('should not be able to create a company with blank CNPJ', () => {
+      company.CNPJ = ''
+      expect(createCompany.execute(company)).rejects.toThrowError('Invalid company CNPJ')
+    })
   })
 })
