2129dbdf544a08e85ca230b0c7b1632ec7c09d66
NathanHermes <hermesnathan8@gmail.com>
2023-10-12 15:11:29 -0300

    :sparkles: Feat(utils): add resolver props interface

--- backend/src/utils/resolver.ts
+++ backend/src/utils/resolver.ts
@@ -4,8 +4,14 @@ export interface HandlerFunction {
   (request: Request, response: Response, next?: NextFunction)
 }
 
+interface ResolverProps {
+  request: Request
+  response: Response
+  next: NextFunction
+}
+
 export function resolver (handlerFunction: HandlerFunction) {
-  return async (request, response, next) => {
+  return async ({ request, response, next }: ResolverProps) => {
     return Promise
       .resolve(handlerFunction(request, response, next))
       .catch((error) => next(error))
