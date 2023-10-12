1ba5fab9f1b2eaedfb7f561b55b86518f5f76875
NathanHermes <hermesnathan8@gmail.com>
2023-10-12 15:09:37 -0300

    :sparkles: Feat(utils): add HandlerFunction interface

--- backend/src/utils/resolver.ts
+++ backend/src/utils/resolver.ts
@@ -1,4 +1,10 @@
-export function resolver (handlerFunction) {
+import { NextFunction, Request, Response } from 'express'
+
+export interface HandlerFunction {
+  (request: Request, response: Response, next?: NextFunction)
+}
+
+export function resolver (handlerFunction: HandlerFunction) {
   return async (request, response, next) => {
     return Promise
       .resolve(handlerFunction(request, response, next))
