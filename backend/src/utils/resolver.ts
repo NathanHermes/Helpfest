9914cdd78c4e0ab88a74712c8aa74b971dd7f00a
NathanHermes <hermesnathan8@gmail.com>
2023-10-16 21:49:29 -0300

    :coffin: Refactor(utils): Update resolver logic

--- backend/src/utils/resolver.ts
+++ backend/src/utils/resolver.ts
@@ -1,19 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
 
-export interface HandlerFunction {
-  (request: Request, response: Response, next?: NextFunction)
-}
-
-interface ResolverProps {
-  request: Request
-  response: Response
-  next: NextFunction
-}
+export type HandlerFunction = (request: Request, response: Response, next?: NextFunction) => Promise<Response | undefined>
 
 export function resolver (handlerFunction: HandlerFunction) {
-  return async ({ request, response, next }: ResolverProps) => {
+  return async (request: Request, response: Response, next?: NextFunction) => {
     return Promise
       .resolve(handlerFunction(request, response, next))
-      .catch((error) => next(error))
+      .catch((error) => next!(error))
   }
 }
