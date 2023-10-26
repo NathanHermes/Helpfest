81d2cc584c05c745e2983a9e9fc09b4abf2b53b7
NathanHermes <hermesnathan8@gmail.com>
2023-10-26 08:13:02 -0300

    feat: create use case class to create a party

---
+++ backend/src/api/domain/use-cases/Party/create-party.ts
@@ -0,0 +1,22 @@
+import { Party, PartyArgs } from '../../entities/Party/party'
+import { PartyRepository } from './party-repository'
+
+export class CreatePartyUseCase {
+  constructor (
+    private repository: PartyRepository
+  ) { }
+
+  async execute ({ name, partyDate, partyTime, celebrities, observations }: PartyArgs): Promise<string | undefined> {
+    const party = new Party({
+      name,
+      partyDate,
+      partyTime,
+      celebrities,
+      observations
+    })
+
+    const uuid = await this.repository.create(party)
+
+    return uuid
+  }
+}
