fe090f29d11f6ab67b3e1f2bc3026d826febae5f
NathanHermes <hermesnathan8@gmail.com>
2023-10-26 08:19:10 -0300

    feat: create a use case class to update a party

---
+++ backend/src/api/domain/use-cases/Party/update-party.ts
@@ -0,0 +1,23 @@
+import { Party, PartyArgs } from '../../entities/Party/party'
+import { PartyRepository } from './party-repository'
+
+export class UpdatePartyUseCase {
+  constructor (
+    private repository: PartyRepository
+  ) { }
+
+  async execute ({ uuid, name, partyDate, partyTime, celebrities, observations }: PartyArgs): Promise<string | undefined> {
+    const party = new Party({
+      uuid,
+      name,
+      partyDate,
+      partyTime,
+      celebrities,
+      observations
+    })
+
+    const _uuid = await this.repository.update(uuid!, party)
+
+    return _uuid
+  }
+}
