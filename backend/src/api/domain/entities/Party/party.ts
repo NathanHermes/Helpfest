0c7906c0ef2d74f6ebc1cac63a672d70012b1034
NathanHermes <hermesnathan8@gmail.com>
2023-10-26 08:11:04 -0300

    feat: create Party class

---
+++ backend/src/api/domain/entities/Party/party.ts
@@ -0,0 +1,40 @@
+export interface PartyArgs {
+  uuid?: string
+  name?: string
+  partyDate?: string
+  partyTime?: string
+  celebrities?: string
+  observations?: string
+}
+
+export class Party {
+  private uuid?: string
+  private name: string
+  private partyDate: string
+  private partyTime: string
+  private celebrities?: string
+  private observations?: string
+
+  constructor (args?: PartyArgs) {
+    this.uuid = args?.uuid
+    this.name = args?.name || ''
+    this.partyDate = args?.partyDate || ''
+    this.partyTime = args?.partyTime || ''
+    this.celebrities = args?.celebrities
+    this.observations = args?.observations
+  }
+
+  get _uuid (): string | undefined { return this.uuid }
+
+  set _uuid (uuid: string) { this.uuid = uuid }
+
+  get _name () { return this.name }
+
+  get _eventDate () { return this.partyDate }
+
+  get _partyTime () { return this.partyTime }
+
+  get _celebrities () { return this.celebrities }
+
+  get _observations () { return this.observations }
+}
