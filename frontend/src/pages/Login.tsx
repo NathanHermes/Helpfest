bcbb574cdcd4e8da86dd490f4923ba8839784a7d
NathanHermes <hermesnathan8@gmail.com>
2023-10-26 22:46:16 -0300

    feat: init Login page develop

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,16 @@
 export const Login = () => {
   return (
-    <>
-      <h1>
-        Testando o login
-      </h1>
-    </>
+    <main className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-black">HELPFEST</h1>
+
+      <section>
+        <h2>Login</h2>
+
+        <fieldset>
+          <span>Email*</span>
+          <input type="email" required />
+        </fieldset>
+      </section>
+    </main>
   )
 }
