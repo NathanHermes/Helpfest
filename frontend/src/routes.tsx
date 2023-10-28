96800e07713fc31b6795149e1373dc0ee84d45ab
NathanHermes <hermesnathan8@gmail.com>
2023-10-28 09:09:58 -0300

    feat: add two new routes | init the login rotine

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import { Login } from './pages/Login'
+import { Home } from './pages/Home'
+import { Loading } from './pages/Loading'
 
 export const routes = createBrowserRouter([
   {
     element: <App />,
     children: [
-      { path: '/', element: <Login /> }
+      { path: '/', element: <Loading /> },
+      { path: '/login', element: <Login /> },
+      { path: '/home', element: <Home /> }
     ]
   }
 ])
