83430437c552d6e75bdfe758c036e71e6537e6e9
NathanHermes <hermesnathan8@gmail.com>
2023-10-28 16:13:18 -0300

    feat: add the login login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+
+import { login } from '@/api/company'
 
 const loginSchema = z.object({
   email: z.string().min(1, 'Campo obrigatório').email('Email inválido'),
@@ -10,6 +13,8 @@ const loginSchema = z.object({
 type LoginFormData = z.infer<typeof loginSchema>
 
 export const Login = () => {
+  const navigate = useNavigate()
+
   const {
     formState: { errors },
     handleSubmit,
@@ -19,38 +24,54 @@ export const Login = () => {
   })
 
   const handleLogin = (data: LoginFormData) => {
-    console.log(data)
+    login(data)
+      .then(() => {
+        localStorage.setItem('isLogged', 'true')
+        navigate('/home')
+      })
+      .catch((error) => {
+        alert(error.response.data.message)
+      })
   }
 
   return (
-    <main className="w-full h-screen flex flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center rounded-md p-4 gap-8">
       <h1 className="text-4xl font-black">HELPFEST</h1>
 
-      <section>
-        <h2>Login</h2>
-
-        <form onSubmit={handleSubmit(handleLogin)}>
-          <fieldset>
-            <span>Email*</span>
-            <input id="email" type="email" {...register('email')} required />
+      <section className='flex flex-col items-center justify-center'>
+        <form onSubmit={handleSubmit(handleLogin)} className='flex flex-col items-center justify-center gap-4'>
+          <fieldset className='flex flex-col gap-1'>
+            <span className='text-sm font-medium'>Email*</span>
+            <input
+              id='email'
+              type="email"
+              {...register('email')}
+              className='w-full h-9 flex border rounded-md bg-transparent px-3 py-1 text-sm placeholder:text-muted-foreground focus:outline focus:outline-purple-600' required />
 
             {errors.email && (
-              <span className="font-medium text-base text-red-500">
+              <span className="font-medium text-sm text-red-500">
                 {errors.email.message}
               </span>
             )}
           </fieldset>
 
-          <fieldset>
-            <span>Senha*</span>
-            <input id="secret" type="password" {...register('secret')} required />
+          <fieldset className='flex flex-col gap-1'>
+            <span className='text-sm font-medium'>Senha*</span>
+            <input
+              id="secret"
+              type="password"
+              {...register('secret')}
+              className='w-full h-9 flex border rounded-md bg-transparent px-3 py-1 text-sm placeholder:text-muted-foreground focus:outline focus:outline-purple-600'
+              required />
 
             {errors.secret && (
-              <span className="font-medium text-base text-red-500">
+              <span className="font-medium text-sm text-red-500">
                 {errors.secret.message}
               </span>
             )}
           </fieldset>
+
+          <button className='w-full h-9 rounded-md bg-purple-600'>Login</button>
         </form>
       </section>
     </main>
