faaa4aa136983fa7a3cc7096d338033c119815b0
NathanHermes <hermesnathan8@gmail.com>
2023-10-30 21:32:19 -0300

    feat: add checkbox to show password

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,11 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { login } from '@/api/company'
+import { Checkbox } from '@/components/ui/checkbox'
+import { useState } from 'react'
 
 const loginSchema = z.object({
   email: z.string().min(1, 'Campo obrigatório').email('Email inválido'),
@@ -14,6 +16,7 @@ type LoginFormData = z.infer<typeof loginSchema>
 
 export const Login = () => {
   const navigate = useNavigate()
+  const [secretInputType, setSecretInputType] = useState('password')
 
   const {
     formState: { errors },
@@ -22,6 +25,13 @@ export const Login = () => {
   } = useForm<LoginFormData>({
     resolver: zodResolver(loginSchema)
   })
+
+  const handleCheckbox = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const ariaChecked = event.currentTarget.ariaChecked
+
+    if (ariaChecked === 'true') setSecretInputType('text')
+    else setSecretInputType('password')
+  }
 
   const handleLogin = (data: LoginFormData) => {
     login(data)
@@ -38,10 +48,10 @@ export const Login = () => {
     <main className="flex flex-col items-center justify-center rounded-md p-4 gap-8">
       <h1 className="text-4xl font-black">HELPFEST</h1>
 
-      <section className='flex flex-col items-center justify-center'>
+      <section className='flex flex-col items-center justify-center gap-8'>
         <form onSubmit={handleSubmit(handleLogin)} className='flex flex-col items-center justify-center gap-4'>
           <fieldset className='flex flex-col gap-1'>
-            <span className='text-sm font-medium'>Email*</span>
+            <label htmlFor='email' className='text-sm font-medium'>Email*</label>
             <input
               id='email'
               type="email"
@@ -56,10 +66,10 @@ export const Login = () => {
           </fieldset>
 
           <fieldset className='flex flex-col gap-1'>
-            <span className='text-sm font-medium'>Senha*</span>
+            <label htmlFor='secret' className='text-sm font-medium'>Senha*</label>
             <input
               id="secret"
-              type="password"
+              type={secretInputType}
               {...register('secret')}
               className='w-full h-9 flex border rounded-md bg-transparent px-3 py-1 text-sm placeholder:text-muted-foreground focus:outline focus:outline-purple-600'
               required />
@@ -71,8 +81,17 @@ export const Login = () => {
             )}
           </fieldset>
 
-          <button className='w-full h-9 rounded-md bg-purple-600'>Login</button>
+          <fieldset className='w-full flex items-center gap-1'>
+            <Checkbox id='showSecret' onClick={handleCheckbox} />
+            <label htmlFor='showSecret'>Mostrar senha</label>
+          </fieldset>
+
+          <button className='w-full h-9 rounded-md bg-violet-600'>Login</button>
         </form>
+
+        <div className='flex items-center justify-center gap-2 text-sm'>
+          <span>Não possui uma conta?</span> <Link to={'/register'} className='underline underline-offset-2 font-bold text-violet-600'>Crie sua conta</Link>
+        </div>
       </section>
     </main>
   )
