9b902d4129d65cb17073eef8fae94600ad6fbbfa
NathanHermes <hermesnathan8@gmail.com>
2023-11-01 19:58:55 -0300

    feat: remove token validation

--- backend/src/routes/company-routes.ts
+++ backend/src/routes/company-routes.ts
@@ -13,7 +13,7 @@ export const CompanyRoutes = Router()
 CompanyRoutes.post('/login', resolver(controller.login))
 CompanyRoutes.post('/company/create', resolver(controller.createCompany))
 
-CompanyRoutes.get('/company/all', resolver(verifyToken.execute), resolver(controller.findAllCompanies))
+CompanyRoutes.get('/company/all', resolver(controller.findAllCompanies))
 CompanyRoutes.get('/company/by', resolver(verifyToken.execute), resolver(controller.findCompanyById))
 CompanyRoutes.put('/company/update', resolver(verifyToken.execute), resolver(controller.updateCompany))
 CompanyRoutes.delete('/company/delete', resolver(verifyToken.execute), resolver(controller.deleteCompany))
