969e91ef4a0988582311fa0f98cedf1893a8996c
NathanHermes <hermesnathan8@gmail.com>
2023-11-01 07:57:47 -0300

    feat: add redirect logic of pages when user is not logged

--- frontend/src/utils/validate-auth.ts
+++ frontend/src/utils/validate-auth.ts
@@ -1,7 +1,9 @@
 import { NavigateFunction, } from 'react-router-dom'
 
-export const validateAuth = async (navigate: NavigateFunction) => {
+export const validateAuth = async (navigate: NavigateFunction, path?: string) => {
   const isLogged = sessionStorage.getItem('isLogged')
 
   if (isLogged) navigate('/home')
+  else if (path !== undefined) navigate(path)
+  else navigate('/login')
 }
