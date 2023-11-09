01d4c1a00167bf096d079bdbcd10867b0a338d49
NathanHermes <hermesnathan8@gmail.com>
2023-11-09 10:58:46 -0300

    :art: style(usecase): reformat UpdateCompanyUseCase constructor and execute method
    Reformat UpdateCompanyUseCase constructor and execute method for consistent code style and formatting.

--- backend/src/api/domain/use-cases/Company/update-company.ts
+++ backend/src/api/domain/use-cases/Company/update-company.ts
@@ -2,11 +2,11 @@ import { Company, CompanyArgs } from '../../entities/company'
 import { CompanyRepository } from './company-repository'
 
 export class UpdateCompanyUseCase {
-  constructor (
+  constructor(
     private repository: CompanyRepository
   ) { }
 
-  async execute ({ uuid, name, email, CNPJ, phone, address, number, city, uf, complement, secret }: CompanyArgs): Promise<string | undefined> {
+  async execute({ uuid, name, email, CNPJ, phone, address, number, city, uf, complement, secret }: CompanyArgs): Promise<string | undefined> {
     const company = new Company({
       uuid,
       name,
