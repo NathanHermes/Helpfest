3566c991baf0386b89ff28a9b0e54d43c10a8b83
NathanHermes <hermesnathan8@gmail.com>
2023-11-08 21:58:07 -0300

    :sparkles: feat(page): add functionality to create a new party
    - Added a function to create a new party.
    - Utilized a dialog component for party creation.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { PartyModel, getAllParties, updateParty } from '@/api/party'
+import { PartyModel, createParty, getAllParties, updateParty } from '@/api/party'
+import { CreatePartyDailog } from '@/components/create-party-dailog'
 import { PartyDailog } from '@/components/party-dailog'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { validateAuth } from '@/utils/validate-auth'
@@ -40,13 +41,26 @@ export const Home = () => {
       })
   }
 
+  const handleCreateParty = (token: string, party: PartyModel) => {
+    createParty(token, party)
+      .then(() => {
+        alert('Evento cadastrado')
+        loadParties()
+      })
+      .catch((error) => {
+        alert(error.response.data.message)
+        loadParties()
+      })
+  }
   return (
     <main className='w-full h-screen flex flex-col items-center gap-10'>
       <nav className='w-3/4 min-h-[8vh] flex items-center justify-between mt-4 p-4 rounded-md bg-zinc-950'>
         <h1 className='text-2xl font-medium text-zinc-50'>HELPFEST</h1>
 
         <div className='flex items-center justify-center gap-4'>
-          <button className='px-4 py-2 rounded-md bg-zinc-900 text-zinc-50 duration-500 hover:bg-zinc-800'>+ Novo evento</button>
+          <CreatePartyDailog
+            action={handleCreateParty}
+          />
 
           <Popover>
             <PopoverTrigger className='p-2 rounded-full bg-zinc-900 duration-500 hover:bg-zinc-800'>
