28bc8be8a46db8e4b0a13f2a205472b65ef0b172
NathanHermes <hermesnathan8@gmail.com>
2023-11-24 12:45:30 -0300

    chore(api): reorganize imports in create-company use case
    Move the import statement for CompanyRepository to the correct location in the file. Also, format the constructor and execute method for better readability.

--- backend/src/api/domain/use-cases/Company/create-company.ts
+++ backend/src/api/domain/use-cases/Company/create-company.ts
@@ -1,14 +1,14 @@
-import { CompanyRepository } from '../../use-cases/Company/company-repository'
 import { Company, CompanyArgs } from '../../entities/company'
+import { CompanyRepository } from '../../use-cases/company/company-repository'
 
 const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
 
 export class CreateCompanyUseCase {
-  constructor (
+  constructor(
     private repository: CompanyRepository
   ) { }
 
-  async execute ({ name, email, CNPJ, phone, address, number, city, uf, complement, secret }: CompanyArgs): Promise<string | undefined> {
+  async execute({ name, email, CNPJ, phone, address, number, city, uf, complement, secret }: CompanyArgs): Promise<string | undefined> {
     if (name === null || name.trim() === '') throw new Error('Invalid company name')
     if (name.length <= 3) throw new Error('Invalid company name length')
 
