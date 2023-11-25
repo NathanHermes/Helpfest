ed2c5748ae56d45b0fed08b2451ef598a08715b2
NathanHermes <hermesnathan8@gmail.com>
2023-11-25 16:07:27 -0300

    feat: make validation asynchronous
    - Refactor the CompanyInputResquestValidator to make the validation method asynchronous.
    - The validation now returns a Promise that resolves to a string indicating an error message if the validation fails.

--- backend/src/api/domain/use-cases/Company/company-input-request-validator.ts
+++ backend/src/api/domain/use-cases/Company/company-input-request-validator.ts
@@ -1,18 +1,10 @@
 import { CompanyArgs } from '../../entities/company'
-import { Notification } from '../utils/validation/notification'
 import { Validator } from '../utils/validation/validator'
 
 export class CompanyInputResquestValidator implements Validator<CompanyArgs> {
-  validate({ name, email, CNPJ, phone, address, number, city, uf, complement, secret }: CompanyArgs): Notification {
-    const notification: Notification = new Notification()
-    
-    if (name === null)
-      notification.addError('Name is null')
-    if (name === undefined)
-      notification.addError('Name is undefined')
-    if (name === '')
-      notification.addError('Name is blank')
-    return notification
+  async validate({ name, email, CNPJ, phone, address, number, city, uf, complement, secret }: CompanyArgs): Promise<string | undefined> {
+    if (name === null) return 'Name is null'
+    if (name === undefined) return 'Name is undefined'
+    if (name.trim() === '') return 'Name is blank'
   }
-
 }
