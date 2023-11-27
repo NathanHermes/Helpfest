1017b1d19362afcdd1dffe948099ad364836762c
NathanHermes <hermesnathan8@gmail.com>
2023-11-27 20:33:38 -0300

    chore: refactor CompanyInputResquestValidator
    - Removed null check for 'name'
    - Removed null check for 'email'
    - Adjusted email validation to check for blank values

--- backend/src/api/domain/use-cases/Company/company-input-request-validator.ts
+++ backend/src/api/domain/use-cases/Company/company-input-request-validator.ts
@@ -3,12 +3,11 @@ import { Validator } from '../utils/validation/validator'
 
 export class CompanyInputResquestValidator implements Validator<CompanyArgs> {
   async validate({ name, email, CNPJ, phone, address, number, city, uf, complement, secret }: CompanyArgs): Promise<string | undefined> {
-    if (name === null) return 'Name is null'
     if (name === undefined) return 'Name is undefined'
     if (name.trim() === '') return 'Name is blank'
     if (name.length <= 3) return 'Name length is invalid'
 
-    if (email === null) return 'Email is null'
     if (email === undefined) return 'Email is undefined'
+    if (email.trim() === '') return 'Email is blank'
   }
 }
