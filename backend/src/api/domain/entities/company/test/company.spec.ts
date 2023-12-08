98b28766211baafdf6b40dde1ccc18c995e9ac7b
NathanHermes <hermesnathan8@gmail.com>
2023-12-08 15:36:05 -0300

    style: move company test class to /company/test/

--- backend/src/api/domain/entities/company/test/company.spec.ts
+++ backend/src/api/domain/entities/company/test/company.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { Company } from './company'
+import { Company } from '../company'
 
 test('should be able to create a object instance of company', () => {
   const company = new Company({
