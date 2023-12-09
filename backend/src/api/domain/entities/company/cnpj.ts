a97bee764920a29238a496fd1123ddf803100ff4
NathanHermes <hermesnathan8@gmail.com>
2023-12-08 21:38:09 -0300

    feat: implement CNPJ formatting
    - Implement a new method in the Cnpj class to format CNPJ strings.

--- backend/src/api/domain/entities/company/cnpj.ts
+++ backend/src/api/domain/entities/company/cnpj.ts
@@ -1,1 +1,22 @@
-export class cnpj { }
+export class cnpj {
+  constructor(private cnpj: string) { }
+
+  public format(): string {
+    const cleanCnpj = this.getOnlyNumbers()
+
+    return cleanCnpj.slice(0, 14).split('').reduce((accumulator, digit, index) => {
+      const result = `${accumulator}${digit}`
+      if (index !== this.cnpj.length - 1) {
+        if (index === 1 || index === 4) return `${result}.`
+        if (index === 7) return `${result}/`
+        if (index === 11) return `${result}-`
+      }
+
+      return result
+    }, '')
+  }
+
+  private getOnlyNumbers(): string {
+    return this.cnpj.replace(/\D/g, '')
+  }
+}
