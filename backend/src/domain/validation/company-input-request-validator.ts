0266535b11c509100e87a5dd4ba29b146b7f9d99
Nathan Hermes <hermesnathan8@gmail.com>
2024-03-10 16:49:14 -0300

    fix(domain): Improve name and CNPJ validation messages and add CNPJ length check

--- backend/src/domain/validation/company-input-request-validator.ts
+++ backend/src/domain/validation/company-input-request-validator.ts
@@ -8,7 +8,7 @@ export class CompanyInputResquestValidator implements Validator<ICompany> {
   validate({ name, email, cnpj, phone, address, number, city, uf, complement, secret }: ICompany): boolean {
     if (name === undefined) this.errors.push('Name is undefined')
     else if (name.trim() === '') this.errors.push('Name is blank')
-    else if (name.length <= 3) this.errors.push('Name length is invalid')
+    else if (name.length <= 3) this.errors.push('Name length is shorter than three characters')
 
     if (email === undefined) this.errors.push('Email is undefined')
     else if (email.trim() === '') this.errors.push('Email is blank')
@@ -16,6 +16,10 @@ export class CompanyInputResquestValidator implements Validator<ICompany> {
 
     if (cnpj === undefined) this.errors.push('cnpj is undefined')
     else if (cnpj.trim() === '') this.errors.push('cnpj is blank')
+    else if (cnpj.trim().length < 11) this.errors.push('cnpj lenght is shorter than eleven characters')
+    else {
+      const test = new cnpj(cnpj)
+    }
 
     return this.errors.length !== 0
   }
