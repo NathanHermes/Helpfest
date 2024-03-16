56898367916817c5e1ced81cc958ed94f0d6e174
Nathan Hermes <hermesnathan8@gmail.com>
2024-03-16 15:05:09 -0300

    feat: Implement CNPJ validation with basic checks and error messages

--- backend/src/domain/models/cnpj.model.ts
+++ backend/src/domain/models/cnpj.model.ts
@@ -1,5 +1,8 @@
 export class Cnpj {
   private readonly regex: RegExp = /\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}/
+  private readonly invalidCnpjs: string[] = [
+    '11111111111111'
+  ]
   private errors: string[] = []
 
   constructor(private readonly cnpj: string | undefined) { }
@@ -7,22 +10,29 @@ export class Cnpj {
   public isValid(): boolean {
     if (this.cnpj === undefined) {
       this.errors.push('Cnpj is undefined')
-    } else if (this.cnpj.trim() === '') {
+    } else if (this.cnpj.trim().length === 0) {
       this.errors.push('Cnpj is blank')
     } else if (this.cnpj.trim().length <= 13) {
       this.errors.push('Cnpj length is shorter than fourteen characters')
-    } else if (this.cnpj.trim().length > 18) {
+    } else if (this.cnpj.trim().length >= 19) {
       this.errors.push('Cnpj length is longer than eighteen characters')
     } else {
-      const digits = this.cnpj.replace(/[^0-9]/g, '')
-      const firstSum = this.calculateSum(digits, 5)
-      const secondSum = this.calculateSum(digits, 6)
+      const digits: string = this.cnpj.replace(/[^0-9]/g, '')
+      const firstSum: number = this.calculateSum(digits, 5)
+      const secondSum: number = this.calculateSum(digits, 6)
 
-      const firstDigit = (firstSum % 11) < 2 ? 0 : 11 - (firstSum % 11)
-      const secondDigit = (secondSum % 11) < 2 ? 0 : 11 - (secondSum % 11)
+      const firstDigit: number = (firstSum % 11) < 2 ? 0 : 11 - (firstSum % 11)
+      const secondDigit: number = (secondSum % 11) < 2 ? 0 : 11 - (secondSum % 11)
+      const isValid: boolean = firstDigit === parseInt(digits.charAt(12)) && secondDigit === parseInt(digits.charAt(13))
 
-      firstDigit === parseInt(digits.charAt(12)) && secondDigit === parseInt(digits.charAt(13))
+      if (this.invalidCnpjs.includes(digits)) {
+        this.errors.push('Cnpj is invalid')
+      } else if (isValid) {
+        this.errors.push('Cnpj is invalid')
+      }
     }
+    console.log(this.errors.length === 0)
+
     return this.errors.length === 0
   }
 
@@ -34,7 +44,7 @@ export class Cnpj {
     return sum
   }
 
-  public getErros(): string[] {
+  public getErrors(): string[] {
     return this.errors
   }
 }
