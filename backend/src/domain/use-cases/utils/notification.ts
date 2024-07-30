1fa70ef46eef1b77fdc441c134933361961e8cf1
Nathan Hermes <hermesnathan8@gmail.com>
2024-07-29 22:11:13 -0300

    feat: Implement Notification class for error handling
    - Create Notification class with private errors array
    - Add methods for error management:
      - addError: Adds a new error message
      - isCorrect: Checks if there are no errors
      - hasErros: Checks if there are any errors
      - errorMessage: Concatenates all error messages

---
+++ backend/src/domain/use-cases/utils/notification.ts
@@ -0,0 +1,23 @@
+export class Notification {
+  #errors: Error[] = []
+
+  public addError( message: string ): void {
+    this.#errors.push( new Error( message ) )
+  }
+
+  public isCorrect(): boolean {
+    return !this.hasErros()
+  }
+
+  public hasErros(): boolean {
+    return this.#errors.length > 0
+  }
+
+  public errorMessage(): string {
+    let errors: string = ''
+
+    this.#errors.map( error => errors += error.message )
+
+    return errors
+  }
+}
