43833a6c8c3ecd9bdb4313214457640ed497b947
Nathan Hermes <hermesnathan8@gmail.com>
2024-07-31 22:41:43 -0300

    style: Rename undefinedOrEmpty method to isUndefinedOrEmpty

--- backend/src/domain/use-cases/utils/validator.ts
+++ backend/src/domain/use-cases/utils/validator.ts
@@ -3,7 +3,7 @@ import { Notification } from './notification'
 export abstract class Validator<T> {
   public abstract validate( type: T ): Notification
 
-  public undefinedOrEmpty( string: string ): boolean {
+  public isUndefinedOrEmpty( string: string ): boolean {
     return string === undefined || string.trim() === ''
   }
 }
