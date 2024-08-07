ea041e441f8bbce132eb9da878d64da96e5564d3
hermesnathan8 <hermesnathan8@gmail.com>
2024-08-07 16:58:06 +0000

    fix: Correct a bug isUndefinedOrEmpty method
    - Update method signature to allow undefined parameter

--- backend/src/domain/use-cases/utils/validator.ts
+++ backend/src/domain/use-cases/utils/validator.ts
@@ -3,7 +3,7 @@ import { Notification } from './notification'
 export abstract class Validator<T> {
   public abstract validate( type: T ): Notification
 
-  public isUndefinedOrEmpty( string: string ): boolean {
+  public isUndefinedOrEmpty( string: string | undefined ): boolean {
     return string === undefined || string.trim() === ''
   }
 }
